cb497ec484bcc1f3b0c9c46d2d65e5367ce9fe73
harsh3536 <harshkhokhar3536@gmail.com>
2024-03-14 14:18:06 +0530

    Update usercontroller.js

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,4 +1,4 @@
-const User = require ("../model/usermodel");
+const User = require("../model/usermodel");
 
 exports.user = async (req, res, next) => {
     const { name, email, password, age } = req.body;
@@ -31,13 +31,38 @@ exports.userlogin = async (req, res, next) => {
     else {
         if (email) {
             res.status(404).json({ message: "Email not Valid" })
-        }else if (password) {
+        } else if (password) {
             res.status(404).json({ message: "Password not Valid" })
         } else if (email && password) {
             res.status(404).json({ message: "Email And Password not Valid" })
-        }else {
+        } else {
             res.status(404).json({ message: "Login UnSuccessful" })
         }
     }
+}
+
+exports.userupdate = async (req, res) => {
+    const userId = await User.findById(req.body.id)
+    const { name, email, password, age } = req.body;
+    if (!userId) {
+        res.status(404).json({ message: "User Not Found" })
+    }
+
+    userupdate = await User.findByIdAndUpdate(req.body.id, { name, email, password, age }, { new: true });
+    res.status(200).json({ success: true })
+}
+
+exports.userdelete = async (req, res) => {
+    const _id = await User.findById(req.body._id)
+    if (!_id) {
+        res.status(404).json({ message: "User Not Found" })
+    }
+    userupdate = await User.findByIdAndDelete(_id);
+    res.status(200).json({ message: "User Data Delete Successfully" })
+}
+
+exports.getData = async (req, res) => {
+    const db =await User.find();
+    res.status(200).json(db)
 }
 
