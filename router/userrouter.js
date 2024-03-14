70c3ca66108d32aa22d91c5b6f61b11bccf7f39b
harsh3536 <harshkhokhar3536@gmail.com>
2024-03-14 14:18:15 +0530

    Update userrouter.js

--- router/userrouter.js
+++ router/userrouter.js
@@ -1,10 +1,13 @@
 const app = require("express");
-const { user, userlogin } = require("../controller/usercontroller");
+const { user, userlogin, userupdate, userdelete, getData } = require("../controller/usercontroller");
 // const app = express()
 const router = app.Router()
 
 // app.use(user)
 router.post("/user", user)
 router.get("/login",userlogin)
+router.put("/update",userupdate)
+router.delete("/delete",userdelete)
+router.get("/getdata",getData)
 
 module.exports = router
